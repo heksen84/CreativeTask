ea3fc71b67e78bc0094d30985e50d5ada7704d73
heksen84 <heksen84@yandex.ru>
2021-09-30 19:01:45 +0600

    Настроил orm doctrine

--- bootstrap.php
+++ bootstrap.php
@@ -12,6 +12,23 @@ use App\Container\Container;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$paths = array("/path/to/entity-files");
+$isDevMode = false;
+
+// the connection configuration
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => '',
+    'dbname'   => 'foo',
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+
 (new Dotenv('APP_ENV'))->loadEnv(__DIR__ . '/.env');
 
 $env = $_ENV['APP_ENV'];
